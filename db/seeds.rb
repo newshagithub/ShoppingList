0470f6b8f1b3a3c356f34c4cdb54788dbdf9f302
Newsha <newsha@umaryland.edu>
2016-04-05 23:56:17 -0400

    Commit to add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,5 +7,8 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 List.delete_all
-Item.delete_all
+Product.delete_all
 
+List.create([{ title: 'Grocery' , description: 'groceries' }, { title: 'Home Improvement', description: 'home improvement' }, { title: 'School Supplies', description: 'school stuff' }])
+
+Product.create([{title: 'chips', list_id: 1, description: 'hot', quantity: 2, price: 2.5 }, {title: 'paint', list_id: 2, description: 'yellow', quantity: 2, price: 2.5},{title: 'pen', list_id: 3, description: 'blue', quantity: 3, price: 1.5 }, {title: 'notebook', list_id: 3, description: 'new design', quantity: 2, price: 5 }])
